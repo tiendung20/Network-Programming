ac6c6659bb244972eb82d13aebd5538464acdb9d
tiendung20 <ntdung2316@gmail.com>
2021-10-13 23:28:19 +0700

    Read file

---
+++ Lec2/Ex2/shareClient.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 4096
+#define SERV_PORT 3000
+
+int main(int argc, char **argv)
+{
+    int sockfd, i;
+    char *s = (char *)malloc(sizeof(char));
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE], recvline[MAXLINE];
+
+    //basic check of the arguments
+    //additional checks can be inserted
+    if (argc != 2)
+    {
+        perror("Usage: TCPClient <IP address of the server>");
+        exit(1);
+    }
+
+    //Create a socket for the client
+    //If sockfd<0 there was an error in the creation of the socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Problem in creating the socket");
+        exit(2);
+    }
+
+    //Creation of the socket
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+    servaddr.sin_port = htons(SERV_PORT); //convert to big-endian order
+
+    //Connection of the client to the socket
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("Problem in connecting to the server");
+        exit(3);
+    }
+    FILE *fp = fopen("qldt.zip", "rb");
+    if (fp == NULL)
+    {
+        printf("ERROR\n");
+        exit(0);
+    }
+    while (!feof(fp))
+    {
+        i = fread(s, sizeof(char), 1, fp);
+        if (i == 0)
+            break;
+        memset(sendline, 0, sizeof(sendline));
+        strcpy(sendline, s);
+        send(sockfd, sendline, sizeof(char), 0);
+
+        if (recv(sockfd, recvline, MAXLINE, 0) == 0)
+        {
+            //error: server terminated prematurely
+            perror("The server terminated prematurely");
+            exit(4);
+        }
+    }
+
+    fclose(fp);
+
+    exit(0);
+}
---
+++ Lec2/Ex2/shareServer.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 4096
+#define SERV_PORT 3000
+#define LISTENQ 8
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd, n;
+    // pid_t childpid;
+    socklen_t clilen;
+    char buf[MAXLINE];
+    struct sockaddr_in cliaddr, servaddr;
+
+    //creation of the socket
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //preparation of the socket address
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    listen(listenfd, LISTENQ);
+
+    printf("%s\n", "Server running...waiting for connections.");
+
+    for (;;)
+    {
+
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+        printf("%s\n", "Received request...");
+        printf("%s\t%d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+        FILE *fp = fopen("file.zip", "wb");
+        if (fp == NULL)
+        {
+            printf("ERROR\n");
+            return 0;
+        }
+        n = 1;
+        while (n > 0)
+        {
+            memset(buf, 0, sizeof(buf));
+            n = recv(connfd, buf, MAXLINE, 0);
+            if (n < 0)
+                break;
+            fwrite(buf, sizeof(char), 1, fp);
+            send(connfd, "buf", strlen("buf"), 0);
+        }
+
+        if (n < 0)
+        {
+            perror("Read error");
+            exit(1);
+        }
+        fclose(fp);
+        close(connfd);
+    }
+    //close listening socket
+    close(listenfd);
+
+    return 0;
+}
