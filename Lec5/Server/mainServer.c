d4e1799ebd717b66bb22e9048ea9754f0cc9cea6
tiendung20 <ntdung2316@gmail.com>
2021-10-25 11:23:32 +0700

    Update Lec5

--- Lec5/Server/mainServer.c
+++ Lec5/Server/mainServer.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <signal.h>
 #include "student_info.h"
 #include "course_schedule.h"
 #include "session.h"
@@ -13,13 +15,15 @@
 #define SERV_PORT 3000
 #define LISTENQ 8
 
+void sig_chld(int);
+
 int main(int argc, char **argv)
 {
     NodeS *root = NULL;
     NodeCourse *rootC = NULL;
     int check = 0;
     int listenfd, connfd;
-    // pid_t childpid;
+    pid_t childpid;
     socklen_t clilen;
     struct sockaddr_in cliaddr, servaddr;
 
@@ -45,10 +49,25 @@ int main(int argc, char **argv)
         printf("%s\n", "Received request...");
         printf("Client's IP: %-20s - Port: %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
 
-        sessionSt(root, rootC, connfd);
+        if ((childpid = fork()) == 0)
+        {
+            close(listenfd);
+            sessionSt(root, rootC, connfd);
+            close(connfd);
+            exit(0);
+        }
+
+        signal(SIGCHLD, sig_chld);
 
         close(connfd);
     }
+}
 
-    close(listenfd);
+void sig_chld(int signo)
+{
+    pid_t pid;
+    int stat;
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0)
+        printf("Child %d terminated\n", pid);
+    return;
 }
